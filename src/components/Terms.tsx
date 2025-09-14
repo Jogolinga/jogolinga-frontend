989df8ae1f7f615ad086a1d8c22f19b9e49a3e47
Jogolinga <ceddoshop@gmail.com>
2025-09-14 13:16:59 +0200

    Update Terms.tsx

--- src/components/Terms.tsx
+++ src/components/Terms.tsx
@@ -170,7 +170,7 @@ const Terms: React.FC = () => {
           </p>
           <div className="bg-gray-50 p-4 rounded-lg">
             <p className="text-gray-700">
-              <strong>Email :</strong> fatouybadjl@gmail.com<br />
+              <strong>Email :</strong> ceddoshop@gmail.com<br />
               <strong>Application :</strong> Jogolinga - Plateforme d'apprentissage de langues africaines
             </p>
           </div>
